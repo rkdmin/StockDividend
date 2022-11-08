4232b2b9bfefd6e04988676c67a3f66c80d16728
22rkdmin <22rkdmin@naver.com>
2022-11-09 01:39:07 +0900

    init

---
+++ src/main/java/com/zerobase/StockDividend/StockDividendApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.StockDividend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockDividendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockDividendApplication.class, args);
+    }
+
+}
